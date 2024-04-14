4e0a27b22454ee372a57055b1b02f84269f736eb
evanbene <evanbene48@gmail.com>
2024-04-14 21:23:48 +0700

    tailwind setup

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+// prettier.config.js
+export default {
+    plugins: [require("prettier-plugin-tailwindcss")],
+  };
+
+//---cjs version
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+//---js version
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
